a5691df50894f4674b1a606540c0ce9e042c716e
JFDomChav <JoseFernandoDom@protonmail.com>
2025-05-25 15:23:33 -0600

    Estructura básica.

---
+++ Formetrica/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Formetrica
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Formetrica/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Formetrica
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
